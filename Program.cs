7ba41afb242af70ff38aa1d3d8dce23e78910c94
Arnaud Couchet <arnaud.couchet@icm-institute.org>
2023-02-23 14:22:44 +0100

    Init.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CS9;
+
+class Program
+{
+    public static void Main()
+    {
+        var phoneNumbers = new string[2];
+        Person person1 = new("Nancy", "Davolio", phoneNumbers);
+        Person person2 = new("Nancy", "Davolio", phoneNumbers);
+        Console.WriteLine(person1 == person2); // output: True
+
+        person1.PhoneNumbers[0] = "555-1234";
+        Console.WriteLine(person1 == person2); // output: True
+
+        Console.WriteLine(ReferenceEquals(person1, person2)); // output: False
+    }
+}
---
+++ RecordOne.cs
@@ -0,0 +1,3 @@
+namespace CS9;
+
+public record Person(string FirstName, string LastName, string[] PhoneNumbers);
